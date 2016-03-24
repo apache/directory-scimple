4c8c9b40ed99039a7793ee0c5a1f93aee54be7ee
Shawn Eion Smith <ses44@psu.edu>
2016-03-24 11:53:16 -0400

    Fleshing out more protocol interfaces

--- scim-api/scim-api-protocol/src/main/java/edu/psu/swe/scim/api/protocol/ResourceTypesResource.java
+++ scim-api/scim-api-protocol/src/main/java/edu/psu/swe/scim/api/protocol/ResourceTypesResource.java
@@ -2,28 +2,52 @@ package edu.psu.swe.scim.api.protocol;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-/*
-From  SCIM Protocol Specification, section 4, page 74
-
-/ResourceTypes
-      An HTTP GET to this endpoint is used to discover the types of
-      resources available on a SCIM service provider (e.g., Users and
-      Groups).  Each resource type defines the endpoints, the core
-      schema URI that defines the resource, and any supported schema
-      extensions.  The attributes defining a resource type can be found
-      in Section 6 of [RFC7643], and an example representation can be
-      found in Section 8.6 of [RFC7643].
-*/
+/**
+ * From SCIM Protocol Specification, section 4, page 74
+ * 
+ * @see <a href="https://tools.ietf.org/html/rfc7644#section-4">Scim spec section 4</a>
+ * 
+ * /ResourceTypes An HTTP GET to this endpoint is used to discover the types of
+ * resources available on a SCIM service provider (e.g., Users and Groups). Each
+ * resource type defines the endpoints, the core schema URI that defines the
+ * resource, and any supported schema extensions. The attributes defining a
+ * resource type can be found in Section 6 of [RFC7643], and an example
+ * representation can be found in Section 8.6 of [RFC7643].
+ * 
+ * In cases where a request is for a specific "ResourceType" or "Schema", the
+ * single JSON object is returned in the same way that a single User or Group is
+ * retrieved, as per Section 3.4.1. When returning multiple ResourceTypes or
+ * Schemas, the message form described by the
+ * "urn:ietf:params:scim:api:messages:2.0:ListResponse" (ListResponse) form
+ * SHALL be used as shown in Figure 3 and in Figure 9 below. Query parameters
+ * described in Section 3.4.2, such as filtering, sorting, and pagination, SHALL
+ * be ignored. If a "filter" is provided, the service provider SHOULD respond
+ * with HTTP status code 403 (Forbidden) to ensure that clients cannot
+ * incorrectly assume that any matching conditions specified in a filter are
+ * true.
+ */
 
 @Path("ResourceTypes")
-public class ResourceTypesResource {
+public interface ResourceTypesResource {
 
   @GET
-  public Response getResourceTypes() {
-    
+  default public Response getAllResourceTypes(@QueryParam("filter") String filter) {
+
+    if (filter != null) {
+      return Response.status(Status.FORBIDDEN).build();
+    }
+
+    return Response.status(Status.NOT_IMPLEMENTED).build();
+  }
+
+  @GET
+  @Path("{id}")
+  default public Response getResourceType(@PathParam("id") String id) {
     return Response.status(Status.NOT_IMPLEMENTED).build();
   }
 }
--- scim-api/scim-api-protocol/src/main/java/edu/psu/swe/scim/api/protocol/UserResource.java
+++ scim-api/scim-api-protocol/src/main/java/edu/psu/swe/scim/api/protocol/UserResource.java
@@ -1,8 +1,49 @@
 package edu.psu.swe.scim.api.protocol;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+/**
+ * From SCIM Protocol Specification, section 3, page 9
+ * 
+ * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.2">Scim spec section 3.2</a>
+ * 
+ * Resource Endpoint         Operations             Description
+   -------- ---------------- ---------------------- --------------------
+   User     /Users           GET (Section 3.4.1),   Retrieve, add,
+                             POST (Section 3.3),    modify Users.
+                             PUT (Section 3.5.1),
+                             PATCH (Section 3.5.2),
+                             DELETE (Section 3.6)
+
+ * @author shawn
+ *
+ */
 
 @Path("Users")
-public class UserResource {
-
+public interface UserResource {
+  
+  /**
+   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.4.1">Scim spec, retrieving known resources</a>
+   * @return
+   */
+  @GET
+  @Path("{id}")
+  default Response getUser(@PathParam("id") String id, @QueryParam("attributes") String attributes) {
+    return Response.status(Status.NOT_IMPLEMENTED).build();
+  }
+  
+  /**
+   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.4.2">Scim spec, query resources</a>
+   * @return
+   */
+  @GET
+  default Response queryForUsers(@QueryParam("attributes") String attributes, @QueryParam("filter") String filter) {
+    return Response.status(Status.NOT_IMPLEMENTED).build();
+  }
+  
 }
