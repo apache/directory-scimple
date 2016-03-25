d13c7cd637f1346c70399a0a80412560aa3cb3e6
Steve Moyer <smoyer1@selesy.com>
2016-03-25 13:50:30 -0400

    Merged in the change to ResourceType's parent class.

--- scim-spec/scim-spec-schema/src/main/java/edu/psu/swe/scim/spec/schema/ResourceType.java
+++ scim-spec/scim-spec-schema/src/main/java/edu/psu/swe/scim/spec/schema/ResourceType.java
@@ -2,14 +2,27 @@ package edu.psu.swe.scim.spec.schema;
 
 import java.util.List;
 
-import edu.psu.swe.scim.spec.resources.ScimResourceWithOptionalId;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlElement;
 
+import edu.psu.swe.scim.spec.resources.ScimResourceWithOptionalId;
+import edu.psu.swe.scim.spec.validator.Urn;
+import lombok.Data;
+
+@Data
 public class ResourceType extends ScimResourceWithOptionalId {
 
   public static final String SCHEMA_URI = "urn:ietf:params:scim:schemas:core:2.0:ResourceType";
 
+  @Data
   public class SchemaExtentionConfiguration {
-    Schema schema;
+    
+    @XmlElement(name = "schema")
+    @Urn
+    @Size(min = 1)
+    String schemaUrn;
+    
+    @XmlElement(name = "required")
     boolean required;
   }
   
@@ -21,7 +34,10 @@ public class ResourceType extends ScimResourceWithOptionalId {
   
   String endpoint;
   
-  Schema schema;
+  @XmlElement(name = "schema")
+  @Urn
+  @Size(min = 1)
+  String schemaUrn;
   
   List<SchemaExtentionConfiguration> schemaExtensions;
 }
