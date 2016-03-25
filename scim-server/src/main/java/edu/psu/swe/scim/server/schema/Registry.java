e2e31bff8105c32c5f7459fa6abb5c278e88190d
Shawn Eion Smith <ses44@psu.edu>
2016-03-25 15:21:33 -0400

    Fleshing out the Schema registry

--- scim-server/src/main/java/edu/psu/swe/scim/server/schema/Registry.java
+++ scim-server/src/main/java/edu/psu/swe/scim/server/schema/Registry.java
@@ -1,5 +1,9 @@
 package edu.psu.swe.scim.server.schema;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,20 +14,36 @@ import javax.ejb.PostActivate;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
+import edu.psu.swe.scim.spec.schema.Schema;
+
 @Singleton
 @Startup
 public class Registry {
-  
+
   private Map<String, String> schemaMap = new HashMap<>();
   private static List<String> schemaNames = new ArrayList<>();
 
-  static {
-    schemaNames.add("enterprise-user-schema");
-  }
-  
   @PostConstruct
   @PostActivate
   private void loadSchemaMap() {
+    for (String s : schemaNames) {
+      String schemaFile = "/schemas/" + s + ".json";
+      InputStream is = Schema.class.getClassLoader().getResourceAsStream(schemaFile);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
+      StringBuilder sb = new StringBuilder();
+
+      String line;
+      try {
+        while ((line = reader.readLine()) != null) {
+          sb.append(line);
+        }
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+      
+      schemaMap.put(s.substring(s.lastIndexOf("/") + 1, s.lastIndexOf(".")), sb.toString());
+    }
   }
 }
