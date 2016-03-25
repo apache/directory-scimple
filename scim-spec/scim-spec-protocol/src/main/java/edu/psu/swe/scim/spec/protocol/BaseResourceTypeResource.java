368cc7b370128637ef43cd91b18776b4269d1c4f
Christopher Harm <crh5255@psu.edu>
2016-03-25 14:07:35 -0400

    Extracting Base ResourceTypeResource interface

---
+++ scim-spec/scim-spec-protocol/src/main/java/edu/psu/swe/scim/spec/protocol/BaseResourceTypeResource.java
@@ -0,0 +1,146 @@
+package edu.psu.swe.scim.spec.protocol;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import edu.psu.swe.scim.spec.protocol.data.SearchRequest;
+import edu.psu.swe.scim.spec.resources.ScimResource;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.jaxrs.PATCH;
+
+@Api("ResourceType")
+public interface BaseResourceTypeResource<T> {
+  
+  /**
+   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.4.1">Scim spec, retrieving known resources</a>
+   * @return
+   */
+  @GET
+  @Path("{id}")
+  @Produces("application/scim+json")
+  @ApiOperation(value="Find by id", response=ScimResource.class, code=200)
+  @ApiResponses(value={
+                  @ApiResponse(code=400, message="Bad Request"),
+                  @ApiResponse(code=404, message="Not found"),
+                  @ApiResponse(code=500, message="Internal Server Error"),
+                  @ApiResponse(code=501, message="Not Implemented")
+                })
+  default Response getById(@ApiParam(value="id", required=true) @PathParam("id") String id, 
+                           @ApiParam(value="attributes", required=false) @QueryParam("attributes") String attributes) {
+    return Response.status(Status.NOT_IMPLEMENTED).build();
+  }
+  
+  /**
+   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.4.2">Scim spec, query resources</a>
+   * @return
+   */
+  @GET
+  @Produces("application/scim+json")
+  @ApiOperation(value="Find by a combination of query parameters", response=ScimResource.class, responseContainer="List", code=200)
+  @ApiResponses(value={
+                  @ApiResponse(code=400, message="Bad Request"),
+                  @ApiResponse(code=404, message="Not found"),
+                  @ApiResponse(code=500, message="Internal Server Error"),
+                  @ApiResponse(code=501, message="Not Implemented")
+                })
+  default Response query(@ApiParam(value="attributes", required=false) @QueryParam("attributes") String attributes, 
+                                 @ApiParam(value="filter", required=false) @QueryParam("filter") String filter,
+                                 @ApiParam(value="sortBy", required=false) @QueryParam("sortBy") String sortBy,
+                                 @ApiParam(value="sortOrder", required=false) @QueryParam("sortOrder") String sortOrder,
+                                 @ApiParam(value="startIndex", required=false) @QueryParam("startIndex") Integer startIndex,
+                                 @ApiParam(value="count", required=false) @QueryParam("count") Integer count) {
+    return Response.status(Status.NOT_IMPLEMENTED).build();
+  }
+  
+  /**
+   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.3">Scim spec, query resources</a>
+   * @return
+   */
+  @POST
+  @Consumes("application/scim+json")
+  @Produces("application/scim+json")
+  @ApiOperation(value="Create", response=ScimResource.class, code=201)
+  @ApiResponses(value={
+      @ApiResponse(code=400, message="Bad Request"),
+      @ApiResponse(code=409, message=ErrorMessageConstants.UNIQUENESS),
+      @ApiResponse(code=500, message="Internal Server Error"),
+      @ApiResponse(code=501, message="Not Implemented")
+    })
+  default Response create(T resource) {
+    return Response.status(Status.NOT_IMPLEMENTED).build();
+  }
+  
+  /**
+   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.4.3">Scim spec, query with post</a>
+   * @return
+   */
+  @POST
+  @Path("/.search")
+  @Produces("application/scim+json")
+  @ApiOperation(value="Search", response=ScimResource.class, responseContainer="List", code=200)
+  @ApiResponses(value={
+      @ApiResponse(code=400, message="Bad Request"),
+      @ApiResponse(code=500, message="Internal Server Error"),
+      @ApiResponse(code=501, message="Not Implemented")
+    })
+  default Response find(SearchRequest request){
+    return Response.status(Status.NOT_IMPLEMENTED).build();
+  }
+  
+  /**
+   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.5.1">Scim spec, update</a>
+   * @return
+   */
+  @PUT
+  @Consumes("application/scim+json")
+  @Produces("application/scim+json")
+  @ApiOperation(value="Update", response=ScimResource.class, code=200)
+  @ApiResponses(value={
+      @ApiResponse(code=400, message="Bad Request"),
+      @ApiResponse(code=500, message="Internal Server Error"),
+      @ApiResponse(code=501, message="Not Implemented")
+    })
+  default Response update(T resource) {
+    return Response.status(Status.NOT_IMPLEMENTED).build();
+  }
+  
+  @PATCH
+  @Consumes("application/scim+json")
+  @Produces("application/scim+json")
+  @ApiOperation(value="Patch a portion of the backing store", code=204)
+  @ApiResponses(value={
+      @ApiResponse(code=400, message="Bad Request"),
+      @ApiResponse(code=404, message="Not found"),
+      @ApiResponse(code=500, message="Internal Server Error"),
+      @ApiResponse(code=501, message="Not Implemented")
+    })
+  default public Response patch() {
+    return Response.status(Status.NOT_IMPLEMENTED).build();
+  }
+  
+  @DELETE
+  @Path("{id}")
+  @ApiOperation(value="Delete from the backing store", code=204)
+  @ApiResponses(value={
+      @ApiResponse(code=400, message="Bad Request"),
+      @ApiResponse(code=404, message="Not found"),
+      @ApiResponse(code=500, message="Internal Server Error"),
+      @ApiResponse(code=501, message="Not Implemented")
+    })
+  default public Response delete(@ApiParam(value="id", required=true) @PathParam("id") String id){
+    return Response.status(Status.NOT_IMPLEMENTED).build();
+  }
+}
--- scim-spec/scim-spec-protocol/src/main/java/edu/psu/swe/scim/spec/protocol/UserResource.java
+++ scim-spec/scim-spec-protocol/src/main/java/edu/psu/swe/scim/spec/protocol/UserResource.java
@@ -1,25 +1,10 @@
 package edu.psu.swe.scim.spec.protocol;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-
-import edu.psu.swe.scim.spec.protocol.data.SearchRequest;
-import edu.psu.swe.scim.spec.resources.ScimUser;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.jaxrs.PATCH;
+
+import javax.ws.rs.Path;
+
+import edu.psu.swe.scim.spec.resources.ScimUser;
 
 //@formatter:off
 /**
@@ -35,132 +20,13 @@ import io.swagger.jaxrs.PATCH;
                              PATCH (Section 3.5.2),
                              DELETE (Section 3.6)
 
- * @author shawn
+ * @author chrisharm
  *
  */
 //@formatter:on
 
 @Path("Users")
-@Api("Users")
-public interface UserResource {
+@Api("ResourceType")
+public interface UserResource extends BaseResourceTypeResource<ScimUser> {
   
-  /**
-   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.4.1">Scim spec, retrieving known resources</a>
-   * @return
-   */
-  @GET
-  @Path("{id}")
-  @Produces("application/scim+json")
-  @ApiOperation(value="Find by user id", response=ScimUser.class, code=200)
-  @ApiResponses(value={
-                  @ApiResponse(code=400, message="Bad Request"),
-                  @ApiResponse(code=404, message="Not found"),
-                  @ApiResponse(code=500, message="Internal Server Error"),
-                  @ApiResponse(code=501, message="Not Implemented")
-                })
-  default Response getUser(@ApiParam(value="id", required=true) @PathParam("id") String id, 
-                           @ApiParam(value="attributes", required=false) @QueryParam("attributes") String attributes) {
-    return Response.status(Status.NOT_IMPLEMENTED).build();
-  }
-  
-  /**
-   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.4.2">Scim spec, query resources</a>
-   * @return
-   */
-  @GET
-  @Produces("application/scim+json")
-  @ApiOperation(value="Find users by a combination of query parameters", response=ScimUser.class, responseContainer="List", code=200)
-  @ApiResponses(value={
-                  @ApiResponse(code=400, message="Bad Request"),
-                  @ApiResponse(code=404, message="Not found"),
-                  @ApiResponse(code=500, message="Internal Server Error"),
-                  @ApiResponse(code=501, message="Not Implemented")
-                })
-  default Response queryForUsers(@ApiParam(value="attributes", required=false) @QueryParam("attributes") String attributes, 
-                                 @ApiParam(value="filter", required=false) @QueryParam("filter") String filter,
-                                 @ApiParam(value="sortBy", required=false) @QueryParam("sortBy") String sortBy,
-                                 @ApiParam(value="sortOrder", required=false) @QueryParam("sortOrder") String sortOrder,
-                                 @ApiParam(value="startIndex", required=false) @QueryParam("startIndex") Integer startIndex,
-                                 @ApiParam(value="count", required=false) @QueryParam("count") Integer count) {
-    return Response.status(Status.NOT_IMPLEMENTED).build();
-  }
-  
-  /**
-   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.3">Scim spec, query resources</a>
-   * @return
-   */
-  @POST
-  @Consumes("application/scim+json")
-  @Produces("application/scim+json")
-  @ApiOperation(value="Create a user", response=ScimUser.class, code=201)
-  @ApiResponses(value={
-      @ApiResponse(code=400, message="Bad Request"),
-      @ApiResponse(code=409, message=ErrorMessageConstants.UNIQUENESS),
-      @ApiResponse(code=500, message="Internal Server Error"),
-      @ApiResponse(code=501, message="Not Implemented")
-    })
-  default Response creatUser(ScimUser user) {
-    return Response.status(Status.NOT_IMPLEMENTED).build();
-  }
-  
-  /**
-   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.4.3">Scim spec, query with post</a>
-   * @return
-   */
-  @POST
-  @Path("/.search")
-  @Produces("application/scim+json")
-  @ApiOperation(value="Search for users", response=ScimUser.class, responseContainer="List", code=200)
-  @ApiResponses(value={
-      @ApiResponse(code=400, message="Bad Request"),
-      @ApiResponse(code=500, message="Internal Server Error"),
-      @ApiResponse(code=501, message="Not Implemented")
-    })
-  default Response findUsers(SearchRequest request){
-    return Response.status(Status.NOT_IMPLEMENTED).build();
-  }
-  
-  /**
-   * @see <a href="https://tools.ietf.org/html/rfc7644#section-3.5.1">Scim spec, update user</a>
-   * @return
-   */
-  @PUT
-  @Consumes("application/scim+json")
-  @Produces("application/scim+json")
-  @ApiOperation(value="Update a user", response=ScimUser.class, code=200)
-  @ApiResponses(value={
-      @ApiResponse(code=400, message="Bad Request"),
-      @ApiResponse(code=500, message="Internal Server Error"),
-      @ApiResponse(code=501, message="Not Implemented")
-    })
-  default Response updateUser(ScimUser user) {
-    return Response.status(Status.NOT_IMPLEMENTED).build();
-  }
-  
-  @PATCH
-  @Consumes("application/scim+json")
-  @Produces("application/scim+json")
-  @ApiOperation(value="Patch a portion of the backing store", code=204)
-  @ApiResponses(value={
-      @ApiResponse(code=400, message="Bad Request"),
-      @ApiResponse(code=404, message="Not found"),
-      @ApiResponse(code=500, message="Internal Server Error"),
-      @ApiResponse(code=501, message="Not Implemented")
-    })
-  default public Response patchUsers() {
-    return Response.status(Status.NOT_IMPLEMENTED).build();
-  }
-  
-  @DELETE
-  @Path("{id}")
-  @ApiOperation(value="Delete a user from the backing store", code=204)
-  @ApiResponses(value={
-      @ApiResponse(code=400, message="Bad Request"),
-      @ApiResponse(code=404, message="Not found"),
-      @ApiResponse(code=500, message="Internal Server Error"),
-      @ApiResponse(code=501, message="Not Implemented")
-    })
-  default public Response deleteUser(@ApiParam(value="id", required=true) @PathParam("id") String id){
-    return Response.status(Status.NOT_IMPLEMENTED).build();
-  }
 }
