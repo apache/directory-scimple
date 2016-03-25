33bbc859a8d558f488e89594a33ecfd4cd6de7c5
Shawn Eion Smith <ses44@psu.edu>
2016-03-25 17:21:17 -0400

    Adding more functionalit to the registry

--- scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/rest/SchemaResourceImpl.java
+++ scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/rest/SchemaResourceImpl.java
@@ -1,11 +1,19 @@
 package edu.psu.swe.scim.server.rest;
 
+import java.util.List;
+
+import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import edu.psu.swe.scim.server.schema.Registry;
 import edu.psu.swe.scim.spec.protocol.SchemaResource;
+import edu.psu.swe.scim.spec.protocol.data.ListResponse;
 
 public class SchemaResourceImpl implements SchemaResource {
+  
+  @Inject
+  Registry registry;
   
   @Override
   public Response getAllSchemas(String filter) {
@@ -14,11 +22,21 @@ public class SchemaResourceImpl implements SchemaResource {
       return Response.status(Status.FORBIDDEN).build();
     }
     
+    ListResponse response = new ListResponse();
+    List<String> schemas = registry.getAllSchemas();
+    
     return Response.status(Status.NOT_IMPLEMENTED).build();
   }
 
   @Override
   public Response getSchema(String uri) {
-    return Response.status(Status.NOT_IMPLEMENTED).build();
+    
+    String schema = registry.getSchemaDoc(uri);
+    
+    if (schema != null){
+      return Response.ok(schema).build();
+    }
+    
+    return Response.status(Status.NOT_FOUND).build();
   }
 }
--- scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/schema/Registry.java
+++ scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/schema/Registry.java
@@ -4,30 +4,81 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.PostActivate;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 
 import edu.psu.swe.scim.spec.schema.Schema;
 import edu.psu.swe.scim.spec.schema.ScimSpecSchema;
+import lombok.extern.slf4j.Slf4j;
 
 @Singleton
 @Startup
+@Slf4j
 public class Registry {
 
-  private Map<String, String> schemaMap = new HashMap<>();
+  private Map<String, String> urnMap = new HashMap<>();
+  private Map<String, Schema> schemaMap = new HashMap<>();
   
-  public String getSchema(String urn){
-    return schemaMap.get(urn);
+  public String getSchemaDoc(String urn){
+    return urnMap.get(urn);
   }
   
-  //TODO - Add validation of the schemaDoc
-  public <T> void addSchema(String urn, T schema, String schemaDoc){
-    schemaMap.put(urn, schemaDoc);
+  public List<String> getAllSchemas(){
+    return new ArrayList<String>(urnMap.values());
+  }
+  
+  public void addSchema(String urn, Schema schema) throws JsonProcessingException{
+    
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    JaxbAnnotationModule jaxbAnnotationModule = new JaxbAnnotationModule();
+    objectMapper.registerModule(jaxbAnnotationModule);
+
+    AnnotationIntrospector jaxbAnnotationIntrospector = new JaxbAnnotationIntrospector();
+    objectMapper.setAnnotationIntrospector(jaxbAnnotationIntrospector);
+
+    String schemaDoc = objectMapper.writeValueAsString(schema);
+    
+    schemaMap.put(urn, schema);
+    urnMap.put(urn, schemaDoc);
+  }
+  
+  public void addSchemaDoc(String urn, String schemaDoc){
+    
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    JaxbAnnotationModule jaxbAnnotationModule = new JaxbAnnotationModule();
+    objectMapper.registerModule(jaxbAnnotationModule);
+
+    AnnotationIntrospector jaxbAnnotationIntrospector = new JaxbAnnotationIntrospector();
+    objectMapper.setAnnotationIntrospector(jaxbAnnotationIntrospector);
+
+    // Unmarshall the JSON document to a Schema and its associated object graph.
+    try {
+      Schema schema = objectMapper.readValue(schemaDoc, Schema.class);
+      urnMap.put(urn, schemaDoc);
+      schemaMap.put(urn, schema);
+      
+    } catch (Throwable t) {
+      log.error("Unexpected Throwable was caught while unmarshalling JSON, schema " + urn + " will not be added: " + t.getLocalizedMessage());
+    }
   }
 
   @PostConstruct
@@ -50,7 +101,7 @@ public class Registry {
         e.printStackTrace();
       }
       
-      schemaMap.put(s.substring(s.lastIndexOf("/") + 1, s.lastIndexOf(".")), sb.toString());
+      urnMap.put(s.substring(s.lastIndexOf("/") + 1, s.lastIndexOf(".")), sb.toString());
     }
   }
 }
