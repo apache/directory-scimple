4a3527d33e374b3aa3afbbdc4632749b9a01f0f9
Steve Moyer <smoyer1@selesy.com>
2016-03-26 20:07:08 -0400

    Added the SCIM - Common module and created a registry for ResourceType objects.

---
+++ scim-common/src/main/java/edu/psu/swe/scim/common/registries/ResourceTypeRegistry.java
@@ -0,0 +1,49 @@
+package edu.psu.swe.scim.common.registries;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import edu.psu.swe.scim.spec.schema.ResourceType;
+
+/**
+ * Provides a registry for ResourceTypes, whether defined in the schema
+ * specification or from implementors new ResourceTypes. In most applications,
+ * this class should be instantiated as a singleton at application start.
+ * 
+ * @author Steve Moyer &lt;smoyer@psu.edu&gt;
+ */
+public class ResourceTypeRegistry {
+
+  Map<String, ResourceType> registry;
+
+  public ResourceTypeRegistry() {
+    registry = new ConcurrentHashMap<>();
+    // TODO - default ResourceTypes from scim-spec-schema
+  }
+
+  /**
+   * Adds a ResourceType to the registry.
+   * 
+   * @param resourceType
+   *          the ResourceType to register.
+   */
+  public void add(ResourceType resourceType) {
+    String key = resourceType.getId();
+    if (key == null) {
+      key = resourceType.getName();
+    }
+    registry.put(key, resourceType);
+  }
+
+  /**
+   * Adds a Set of ResourceType objects to the registry.
+   * 
+   * @param resourceTypeSet
+   *          the ResourceType objects to register.
+   */
+  public void addAll(Set<ResourceType> resourceTypeSet) {
+    resourceTypeSet.forEach(r -> add(r));
+  }
+
+}
