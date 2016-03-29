5817a11a7e6f5ed06bac516bac4e2285dfb4b469
Shawn Eion Smith <ses44@psu.edu>
2016-03-29 16:10:39 -0400

    Adding the last 4 models

---
+++ scim-spec/scim-spec-schema/src/main/java/edu/psu/swe/scim/spec/resources/Email.java
@@ -0,0 +1,11 @@
+package edu.psu.swe.scim.spec.resources;
+
+import edu.psu.swe.scim.spec.schema.SimpleTypedMultiValuedAttribute;
+
+/**
+ * Scim core schema, <a href="https://tools.ietf.org/html/rfc7643#section-4.1.2>section 4.1.2</a>
+ *
+ */
+public class Email extends SimpleTypedMultiValuedAttribute {
+
+}
---
+++ scim-spec/scim-spec-schema/src/main/java/edu/psu/swe/scim/spec/resources/X509Certificate.java
@@ -0,0 +1,11 @@
+package edu.psu.swe.scim.spec.resources;
+
+import edu.psu.swe.scim.spec.schema.SimpleMultiValuedAttribute;
+
+/**
+ * Scim core schema, <a href="https://tools.ietf.org/html/rfc7643#section-4.1.2>section 4.1.2</a>
+ *
+ */
+public class X509Certificate extends SimpleMultiValuedAttribute {
+
+}
