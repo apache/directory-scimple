0d9a9c0d451ea9d83d8e7a34ce75b2fd0d267883
Christopher Harm <crh5255@psu.edu>
2016-03-31 10:25:04 -0400

    Fixing Jackson errors

--- scim-spec/scim-spec-protocol/src/main/java/edu/psu/swe/scim/spec/protocol/ScimApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.psu.swe.scim.spec.protocol;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("v2")
-public class ScimApplication extends Application {
-
-}
