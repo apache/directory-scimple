76ce518d632114d1c8a324aa5ee58f19024e324e
Shawn Eion Smith <ses44@psu.edu>
2016-04-01 12:58:41 -0400

    Adding ScimAttribute extensions to ScimUser

--- scim-spec/scim-spec-schema/src/main/java/edu/psu/swe/scim/spec/resources/ScimUser.java
+++ scim-spec/scim-spec-schema/src/main/java/edu/psu/swe/scim/spec/resources/ScimUser.java
@@ -6,82 +6,105 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 
+import edu.psu.swe.scim.spec.annotation.ScimAttribute;
 import edu.psu.swe.scim.spec.annotation.ScimResourceType;
 import edu.psu.swe.scim.spec.schema.ResourceReference;
+import edu.psu.swe.scim.spec.schema.Schema.Attribute.Returned;
+import edu.psu.swe.scim.spec.schema.Schema.Attribute.Type;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-@ScimResourceType(name="User", schemaList={ScimUser.SCHEMA_URI})
+@ScimResourceType(name="User", schema = ScimUser.SCHEMA_URI, desription="Top level ScimUser", endpoint = "/Users", id="User")
 @XmlRootElement(name = "User")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ScimUser extends ScimResource {
 
   public static final String SCHEMA_URI = "urn:ietf:params:scim:schemas:core:2.0:User";
 
+  @ScimAttribute
   @XmlElement(name = "active")
   boolean active = true;
   
+  @ScimAttribute(canonicalValues={"home", "work", "other"}, referenceTypes={Type.COMPLEX})
   @XmlElement(name = "addresses")
   List<Address> addresses;
   
+  @ScimAttribute
   @XmlElement(name = "displayName")
   String displayName;
   
+  @ScimAttribute(canonicalValues={"home", "work", "other"}, referenceTypes={Type.COMPLEX})
   @XmlElement(name = "emails")
   List<Email> emails;
   
-    @XmlElement(name = "entitlements")
+  @ScimAttribute(referenceTypes={Type.COMPLEX})
+  @XmlElement(name = "entitlements")
   List<Entitlement> entitlements;
   
+  @ScimAttribute(referenceTypes={Type.COMPLEX})
   @XmlElement(name = "groups")
   List<ResourceReference> groups;
   
+  @ScimAttribute(referenceTypes={Type.COMPLEX})
   @XmlElement(name = "ims")
   List<Im> ims;
   
+  @ScimAttribute
   @XmlElement(name = "locale")
   String locale;
   
+  @ScimAttribute(referenceTypes={Type.COMPLEX})
   @XmlElement(name = "name")
   Name name;
   
+  @ScimAttribute
   @XmlElement(name = "nickName")
   String nickName;
   
+  @ScimAttribute(returned=Returned.NEVER)
   @XmlElement(name = "password")
   String password;
   
+  @ScimAttribute(referenceTypes={Type.COMPLEX})
   @XmlElement(name = "phoneNumbers")
   List<PhoneNumber> phoneNumbers;
   
+  @ScimAttribute(referenceTypes={Type.COMPLEX})
   @XmlElement(name = "photos")
   List<Photo> photos;
 
+  @ScimAttribute
   @XmlElement(name = "profileUrl")
   String profileUrl;
   
+  @ScimAttribute
   @XmlElement(name = "preferredLanguage")
   String preferredLanguage;
   
-//  @XmlElement(name = "roles")
-//  List<ScimRole> roles;
+  @ScimAttribute(referenceTypes={Type.COMPLEX})
+  @XmlElement(name = "roles")
+  List<Role> roles;
   
+  @ScimAttribute
   @XmlElement(name = "timezone")
   String timezone;
   
+  @ScimAttribute
   @XmlElement(name = "title")
   String title;
   
+  @ScimAttribute
   @XmlElement(name = "userName")
   String userName;
   
+  @ScimAttribute
   @XmlElement(name = "userType")
   String userType;
   
+  @ScimAttribute(referenceTypes={Type.COMPLEX})
   @XmlElement(name = "x509Certificates")
   List<X509Certificate> x509Certificates;
   
