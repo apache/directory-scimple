80787baecf55c5c370d3077dc3471048046bdce4
Alex Haskell <aph12@psu.edu>
2016-04-15 13:55:06 -0400

    Handling exceptions thrown from provider

--- scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/rest/BulkResourceImpl.java
+++ scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/rest/BulkResourceImpl.java
@@ -8,6 +8,9 @@ import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import edu.psu.swe.scim.server.exception.UnableToCreateResourceException;
+import edu.psu.swe.scim.server.exception.UnableToDeleteResourceException;
+import edu.psu.swe.scim.server.exception.UnableToUpdateResourceException;
 import edu.psu.swe.scim.server.provider.Provider;
 import edu.psu.swe.scim.server.provider.ProviderRegistry;
 import edu.psu.swe.scim.server.schema.Registry;
@@ -25,12 +28,15 @@ import lombok.extern.slf4j.Slf4j;
 public class BulkResourceImpl implements BulkResource {
   private static final Status METHOD_NOT_ALLOWED_STATUS = new Status();
   private static final Status OKAY_STATUS = new Status();
+  private static final Status INTERNAL_SERVER_ERROR_STATUS = new Status();
   private static final String METHOD_NOT_ALLOWED = "405";
   private static final String OKAY = "200";
+  private static final String INTERNAL_SERVER_ERROR = "500";
 
   static {
     METHOD_NOT_ALLOWED_STATUS.setCode(METHOD_NOT_ALLOWED);
     OKAY_STATUS.setCode(OKAY);
+    INTERNAL_SERVER_ERROR_STATUS.setCode(INTERNAL_SERVER_ERROR);
   }
 
   @Inject
@@ -47,50 +53,59 @@ public class BulkResourceImpl implements BulkResource {
 
     for (BulkOperation bulkOperation : request.getOperations()) {
       BulkOperation operationResult = new BulkOperation();
-      ScimResource scimResource = bulkOperation.getData();
-      @SuppressWarnings("unchecked")
-      Class<ScimResource> scimResourceClass = (Class<ScimResource>) scimResource.getClass();
-      Provider<ScimResource> provider = providerRegistry.getProvider(scimResourceClass);
+      try {
+        ScimResource scimResource = bulkOperation.getData();
+        @SuppressWarnings("unchecked")
+        Class<ScimResource> scimResourceClass = (Class<ScimResource>) scimResource.getClass();
+        Provider<ScimResource> provider = providerRegistry.getProvider(scimResourceClass);
 
-      operationResult.setStatus(OKAY_STATUS);
+        operationResult.setStatus(OKAY_STATUS);
 
-      switch (bulkOperation.getMethod()) {
-      case POST: {
-        log.debug("POST: {}", scimResource);
+        switch (bulkOperation.getMethod()) {
+        case POST: {
+          log.debug("POST: {}", scimResource);
 
-        ScimResource newResource = provider.create(scimResource);
-        String bulkOperationPath = bulkOperation.getPath();
-        String newResourceId = newResource.getId();
-        String newResourceUri = uriInfo.getBaseUriBuilder().path(bulkOperationPath).path(newResourceId).build().toString();
+          ScimResource newResource = provider.create(scimResource);
+          String bulkOperationPath = bulkOperation.getPath();
+          String newResourceId = newResource.getId();
+          String newResourceUri = uriInfo.getBaseUriBuilder().path(bulkOperationPath).path(newResourceId).build().toString();
 
-        operationResult.setLocation(newResourceUri);
-      } break;
+          operationResult.setLocation(newResourceUri);
+        } break;
 
-      case DELETE: {
-        log.debug("DELETE: {}", scimResource);
+        case DELETE: {
+          log.debug("DELETE: {}", scimResource);
 
-        String scimResourceId = scimResource.getId();
+          String scimResourceId = scimResource.getId();
 
-        provider.delete(scimResourceId);
-      } break;
+          provider.delete(scimResourceId);
+        } break;
 
-      case PATCH:
-      case PUT: {
-        log.debug("PUT/PATCH: {}", scimResource);
+        case PATCH:
+        case PUT: {
+          log.debug("PUT/PATCH: {}", scimResource);
 
-        ScimResource newResource = provider.update(scimResource);
+          ScimResource newResource = provider.update(scimResource);
 
-        operationResult.setLocation(uriInfo.getBaseUriBuilder().path(bulkOperation.getPath()).path(newResource.getId()).toString());
-      } break;
+          operationResult.setLocation(uriInfo.getBaseUriBuilder().path(bulkOperation.getPath()).path(newResource.getId()).toString());
+        } break;
 
-      default: {
+        default: {
+          ErrorResponse error = new ErrorResponse();
+
+          error.setStatus(METHOD_NOT_ALLOWED);
+          error.setDetail("Method not allowed: " + bulkOperation.getMethod());
+          operationResult.setResponse(error);
+          operationResult.setStatus(METHOD_NOT_ALLOWED_STATUS);
+        } break;
+        }
+      } catch (UnableToCreateResourceException | UnableToDeleteResourceException | UnableToUpdateResourceException resourceException) {
         ErrorResponse error = new ErrorResponse();
 
-        error.setStatus(METHOD_NOT_ALLOWED);
-        error.setDetail("Method not allowed: " + bulkOperation.getMethod());
+        error.setStatus(INTERNAL_SERVER_ERROR);
+        error.setDetail(resourceException.getLocalizedMessage());
         operationResult.setResponse(error);
-        operationResult.setStatus(METHOD_NOT_ALLOWED_STATUS);
-      } break;
+        operationResult.setStatus(INTERNAL_SERVER_ERROR_STATUS);
       }
       completedOperations.add(operationResult);
     }
