ce9bb3d7bc7eebc27119133dcc92322b460f73d5
Steve Moyer <smoyer1@selesy.com>
2016-04-26 10:39:59 -0400

    Moved the values for the default (generated) user into constants, eliminating a magic number that Sonar was complaining about.  Also added javadocs.

--- scim-server/scim-server-example/scim-server-memory/src/main/java/edu/psu/swe/scim/memory/service/InMemoryUserService.java
+++ scim-server/scim-server-example/scim-server-memory/src/main/java/edu/psu/swe/scim/memory/service/InMemoryUserService.java
@@ -20,27 +20,41 @@ import edu.psu.swe.scim.spec.resources.Email;
 import edu.psu.swe.scim.spec.resources.ScimExtension;
 import edu.psu.swe.scim.spec.resources.ScimUser;
 
+/**
+ * Creates a singleton (effectively) Provider<User> with a memory-based
+ * persistence layer.
+ * 
+ * @author Chris Harm &lt;crh5255@psu.edu&gt;
+ */
 @Named
 @ApplicationScoped
 public class InMemoryUserService implements Provider<ScimUser> {
+  
+  static final String DEFAULT_USER_ID = "1";
+  static final String DEFAULT_USER_EXTERNAL_ID = "e" + DEFAULT_USER_ID;
+  static final String DEFAULT_USER_DISPLAY_NAME = "User " + DEFAULT_USER_ID;
+  static final String DEFAULT_USER_EMAIL_VALUE = "e1@example.com";
+  static final String DEFAULT_USER_EMAIL_TYPE = "work";
+  static final int DEFAULT_USER_LUCKY_NUMBER = 7;
 
   private Map<String, ScimUser> users = new HashMap<>();
   
   @PostConstruct
   public void init() {
     ScimUser user = new ScimUser();
-    user.setId("1");
-    user.setExternalId("e1");
-    user.setDisplayName("User 1");
+    user.setId(DEFAULT_USER_ID);
+    user.setExternalId(DEFAULT_USER_EXTERNAL_ID);
+    user.setUserName(DEFAULT_USER_EXTERNAL_ID);
+    user.setDisplayName(DEFAULT_USER_DISPLAY_NAME);
     Email email = new Email();
-    email.setDisplay("e1@example.com");
-    email.setValue("e1@example.com");
-    email.setType("work");
+    email.setDisplay(DEFAULT_USER_EMAIL_VALUE);
+    email.setValue(DEFAULT_USER_EMAIL_VALUE);
+    email.setType(DEFAULT_USER_EMAIL_TYPE);
     email.setPrimary(true);
     user.setEmails(Arrays.asList(email));
     
     LuckyNumberExtension luckyNumberExtension = new LuckyNumberExtension();
-    luckyNumberExtension.setLuckyNumber(7);
+    luckyNumberExtension.setLuckyNumber(DEFAULT_USER_LUCKY_NUMBER);
     
     try {
       user.addExtension(luckyNumberExtension);
@@ -52,35 +66,54 @@ public class InMemoryUserService implements Provider<ScimUser> {
     users.put(user.getId(), user);
   }
   
+  /**
+   * @see edu.psu.swe.scim.server.provider.Provider#create(java.lang.Object)
+   */
   @Override
   public ScimUser create(ScimUser resource) {
     users.put(resource.getId(), resource);
     return resource;
   }
 
+  /**
+   * @see edu.psu.swe.scim.server.provider.Provider#update(java.lang.Object)
+   */
   @Override
   public ScimUser update(ScimUser resource) {
     users.put(resource.getId(), resource);
     return resource;
   }
 
+  /**
+   * @see edu.psu.swe.scim.server.provider.Provider#get(java.lang.String)
+   */
   @Override
   public ScimUser get(String id) {
     return users.get(id);
   }
 
+  /**
+   * @see edu.psu.swe.scim.server.provider.Provider#delete(java.lang.String)
+   */
   @Override
   public void delete(String id) {
     users.remove(id);
   }
 
+  /**
+   * @see edu.psu.swe.scim.server.provider.Provider#find(edu.psu.swe.scim.spec.protocol.search.Filter, edu.psu.swe.scim.spec.protocol.search.PageRequest, edu.psu.swe.scim.spec.protocol.search.SortRequest)
+   */
   @Override
   public List<ScimUser> find(Filter filter, PageRequest pageRequest, SortRequest sortRequest) {
     return new ArrayList<ScimUser>(users.values());
   }
 
+  /**
+   * @see edu.psu.swe.scim.server.provider.Provider#getExtensionList()
+   */
   @Override
   public List<Class<? extends ScimExtension>> getExtensionList() {
     return Arrays.asList(LuckyNumberExtension.class);
   }
+
 }
