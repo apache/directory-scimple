037e42c15a030cd459c9dac363e934d2bcd88264
Steve Moyer <smoyer1@selesy.com>
2016-04-27 17:29:30 -0400

    Made this class final as suggested by Sonar and added javadocs.

--- scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/rest/ScimResourceHelper.java
+++ scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/rest/ScimResourceHelper.java
@@ -3,12 +3,25 @@ package edu.psu.swe.scim.server.rest;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ScimResourceHelper {
+/**
+ * Provides the SCIM defined set of end-points and resources without declaring
+ * a JAX-RS application.  Additional end-points and extensions can be added
+ * by the implementing class.
+ * 
+ * @author Chris Harm &lt;crh5255@psu.edu&gt;
+ */
+public final class ScimResourceHelper {
 
   private ScimResourceHelper() {
-    
+    // Make this a utility class
   }
   
+  /**
+   * Provides a set of JAX-RS annotated classes for the basic SCIM protocol
+   * functionality.
+   * 
+   * @return the JAX-RS annotated classes.
+   */
   public static Set<Class<?>> getScimClassesToLoad(){
     Set<Class<?>> clazzez = new HashSet<>();
     
