0fab20d51104285b396f4fd8b6145e7ed38d3975
Shawn Eion Smith <ses44@psu.edu>
2016-04-28 10:03:37 -0400

    Fixing sonar issues

--- scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/rest/GroupResourceImpl.java
+++ scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/rest/GroupResourceImpl.java
@@ -4,21 +4,12 @@
 package edu.psu.swe.scim.server.rest;
 
 import javax.inject.Inject;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import edu.psu.swe.scim.server.provider.Provider;
 import edu.psu.swe.scim.server.provider.ProviderRegistry;
 import edu.psu.swe.scim.spec.protocol.GroupResource;
-import edu.psu.swe.scim.spec.protocol.data.SearchRequest;
 import edu.psu.swe.scim.spec.resources.ScimGroup;
-import edu.psu.swe.scim.spec.resources.ScimUser;
-import edu.psu.swe.scim.spec.schema.ErrorResponse;
 
-/**
- * @author shawn
- *
- */
 public class GroupResourceImpl extends BaseResourceTypeResourceImpl<ScimGroup> implements GroupResource {
 
   @Inject
