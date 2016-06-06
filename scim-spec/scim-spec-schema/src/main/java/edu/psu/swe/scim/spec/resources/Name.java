3721fba7c7e71486ae0482271a7d97ad450248ed
Shawn Eion Smith <ses44@psu.edu>
2016-06-06 10:32:08 -0400

    Making Name a keyed resource to support meta extensions

--- scim-spec/scim-spec-schema/src/main/java/edu/psu/swe/scim/spec/resources/Name.java
+++ scim-spec/scim-spec-schema/src/main/java/edu/psu/swe/scim/spec/resources/Name.java
@@ -20,7 +20,7 @@ import lombok.Data;
     "honorificSuffix"    
 })
 @XmlAccessorType(XmlAccessType.NONE)
-public class Name implements Serializable {
+public class Name extends KeyedResource implements Serializable  {
 
   private static final long serialVersionUID = -2761413543859555141L;
 
