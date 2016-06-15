a348a8a5c444c6b8b33d027f548945ac69ae77ac
Shawn Eion Smith <ses44@psu.edu>
2016-06-15 12:22:19 -0400

    Modified EndpointUtil to be more generically applicable

--- scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/utility/EndpointUtil.java
+++ scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/utility/EndpointUtil.java
@@ -3,18 +3,33 @@ package edu.psu.swe.scim.server.utility;
 import java.net.URI;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
+import edu.psu.swe.scim.server.provider.ProviderRegistry;
+import edu.psu.swe.scim.spec.resources.ScimResource;
 import lombok.Data;
 
 @RequestScoped
 @Data
 public class EndpointUtil {
-  private URI groupEndpoint;
-  private URI userEndpoint;
+  private UriBuilder baseUri;
+  
+  @Inject
+  ProviderRegistry registry;
+  
+  public URI getEndpoint(Class<? extends ScimResource> resource) {
+    
+    URI uri = null;
+    if (registry.getProvider(resource) != null) {
+      uri = baseUri.path(resource.getSimpleName()).build();
+    }
+    
+    return uri;
+  }
   
   public void process(UriInfo uriInfo) {
-    groupEndpoint = uriInfo.getBaseUriBuilder().path("Groups").build();
-    userEndpoint = uriInfo.getBaseUriBuilder().path("Users").build();
+    baseUri = uriInfo.getBaseUriBuilder();
   }
 }
