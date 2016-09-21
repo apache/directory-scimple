9a7b36f2668a278efb62eb89fd28970c22b064e8
Shawn Eion Smith <ses44@psu.edu>
2016-09-21 13:48:09 -0400

    Adding post processing annotation for client security

---
+++ scim-server/scim-server-common/src/main/java/edu/psu/swe/scim/server/provider/annotations/ScimResourceAttributeFilter.java
@@ -0,0 +1,11 @@
+package edu.psu.swe.scim.server.provider.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface ScimResourceAttributeFilter {
+}
