db2faa9bae7bc9f837bfbd671853f374ad46a55c
Steve Moyer <smoyer@psu.edu>
2018-05-10 12:13:34 -0400

    Logs creating a filter at debug level - resolves #42

--- scim-spec/scim-spec-protocol/src/main/java/edu/psu/swe/scim/spec/protocol/search/Filter.java
+++ scim-spec/scim-spec-protocol/src/main/java/edu/psu/swe/scim/spec/protocol/search/Filter.java
@@ -34,12 +34,12 @@ public class Filter {
   }
 
   public Filter(String filter) throws FilterParseException {
-    log.info("Creating a filter - " + filter);
+    log.debug("Creating a filter - " + filter);
     setFilter(filter);
   }
   
   public Filter(FilterExpression filterExpression) {
-    log.info("Creating a filter - " + filterExpression.toString());
+    log.debug("Creating a filter - " + filterExpression.toString());
     expression = filterExpression;
     this.filter = filterExpression.toString();
   }
