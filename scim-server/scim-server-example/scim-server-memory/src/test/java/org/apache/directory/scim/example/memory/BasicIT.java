fdcf813d93e61c772bd0aa56d187dfd1bb5c890d
Brian Demers <bdemers@apache.org>
2018-08-07 19:51:59 -0400

    Start using Arquillian for ITs

---
+++ scim-server/scim-server-example/scim-server-memory/src/test/java/org/apache/directory/scim/example/memory/BasicIT.java
@@ -0,0 +1,38 @@
+package org.apache.directory.scim.example.memory;
+
+import io.restassured.RestAssured;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.importer.ZipImporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.net.URL;
+
+@RunWith(Arquillian.class)
+public class BasicIT {
+
+  @ArquillianResource
+  private URL url;
+
+  @Deployment(testable = false)
+  public static WebArchive createDeployment() {
+    return ShrinkWrap.create(ZipImporter.class, "ROOT.war")
+        .importFrom(new File("target/scim-server-memory-2.23-SNAPSHOT.war"))
+        .as(WebArchive.class);
+  }
+
+  @Test
+  public void foo() {
+    System.out.println("wtf: " + url);
+
+    System.out.println(RestAssured.given().log().everything().get(url + "/v2/Users").asString());
+
+
+  }
+}
