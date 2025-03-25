e7e0afa26d1970841cc30184a3fac93aed452bdc
Sergii Ripa <sergii.ripa@symphony.com>
2025-03-25 16:36:35 +0100

    Add 'dataSource' field to entitlement, non standard scim attribute (#8)

--- scim-spec/scim-spec-schema/src/main/java/org/apache/directory/scim/spec/resources/Entitlement.java
+++ scim-spec/scim-spec-schema/src/main/java/org/apache/directory/scim/spec/resources/Entitlement.java
@@ -1,34 +1,33 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
- 
-* http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
 
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package org.apache.directory.scim.spec.resources;
-
-import java.io.Serializable;
 
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlType;
-
-import org.apache.directory.scim.spec.annotation.ScimAttribute;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.apache.directory.scim.spec.annotation.ScimAttribute;
+
+import java.io.Serializable;
 
 /**
  * Scim core schema, <a href="https://tools.ietf.org/html/rfc7643#section-4.1.2">section 4.1.2</a>
@@ -37,24 +36,28 @@ import lombok.EqualsAndHashCode;
 @XmlType
 @XmlAccessorType(XmlAccessType.NONE)
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 public class Entitlement implements Serializable, TypedAttribute {
 
   private static final long serialVersionUID = -5657063764529902479L;
 
-  @XmlElement(nillable=true)
-  @ScimAttribute(description="A label indicating the attribute's function.")
+  @XmlElement(nillable = true)
+  @ScimAttribute(description = "A label indicating the attribute's function.")
   String type;
-  
+
   @XmlElement
-  @ScimAttribute(description="The value of an entitlement.")
+  @ScimAttribute(description = "The value of an entitlement.")
   String value;
-  
+
   @XmlElement
-  @ScimAttribute(description="A human readable name, primarily used for display purposes. READ-ONLY.")
+  @ScimAttribute(description = "A human readable name, primarily used for display purposes. READ-ONLY.")
   String display;
-  
+
   @XmlElement
-  @ScimAttribute(description="A Boolean value indicating the 'primary' or preferred attribute value for this attribute, e.g. the preferred mailing address or primary e-mail address. The primary attribute value 'true' MUST appear no more than once.")
+  @ScimAttribute(description = "A Boolean value indicating the 'primary' or preferred attribute value for this attribute, e.g. the preferred mailing address or primary e-mail address. The primary attribute value 'true' MUST appear no more than once.")
   Boolean primary = false;
+
+  @XmlElement(nillable = true)
+  @ScimAttribute(description = "Specifies the source of the entitlement, indicating the data provider responsible for provisioning the user. This is not a standard SCIM2 attribute.")
+  String dataSource;
 }
