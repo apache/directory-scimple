6d3b50f2db2f1aa8c8fdaf4db8d073034332eb4f
Brian Demers <bdemers@apache.org>
2025-12-18 15:46:27 -0500

    Add Jersey 4 example and tests
    Uses Jakarta 11
    
    # Conflicts:
    #	pom.xml

---
+++ scim-server-examples/scim-server-jersey-4/src/main/java/org/apache/directory/scim/example/jersey4/service/InMemoryUserService.java
@@ -0,0 +1,209 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.directory.scim.example.jersey4.service;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.ws.rs.core.Response;
+import org.apache.directory.scim.core.repository.ETag;
+import org.apache.directory.scim.core.repository.PatchHandler;
+import org.apache.directory.scim.core.repository.Repository;
+import org.apache.directory.scim.core.schema.SchemaRegistry;
+import org.apache.directory.scim.example.jersey4.extensions.LuckyNumberExtension;
+import org.apache.directory.scim.server.exception.UnableToCreateResourceException;
+import org.apache.directory.scim.spec.exception.ResourceException;
+import org.apache.directory.scim.spec.exception.ResourceNotFoundException;
+import org.apache.directory.scim.spec.extension.EnterpriseExtension;
+import org.apache.directory.scim.spec.filter.Filter;
+import org.apache.directory.scim.spec.filter.FilterExpressions;
+import org.apache.directory.scim.spec.filter.FilterResponse;
+import org.apache.directory.scim.spec.filter.PageRequest;
+import org.apache.directory.scim.spec.filter.SortRequest;
+import org.apache.directory.scim.spec.filter.attribute.AttributeReference;
+import org.apache.directory.scim.spec.patch.PatchOperation;
+import org.apache.directory.scim.spec.resources.Email;
+import org.apache.directory.scim.spec.resources.Name;
+import org.apache.directory.scim.spec.resources.ScimExtension;
+import org.apache.directory.scim.spec.resources.ScimResource;
+import org.apache.directory.scim.spec.resources.ScimUser;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+/**
+ * Creates a singleton (effectively) Repository<ScimUser> with a memory-based
+ * persistence layer.
+ *
+ * @author Chris Harm &lt;crh5255@psu.edu&gt;
+ */
+@Named
+@ApplicationScoped
+public class InMemoryUserService implements Repository<ScimUser> {
+
+  static final String DEFAULT_USER_ID = UUID.randomUUID().toString();
+  static final String DEFAULT_USER_EXTERNAL_ID = "e" + DEFAULT_USER_ID;
+  static final String DEFAULT_USER_DISPLAY_NAME = "User " + DEFAULT_USER_ID;
+  static final String DEFAULT_USER_EMAIL_VALUE = "e1@example.com";
+  static final String DEFAULT_USER_EMAIL_TYPE = "work";
+  static final int DEFAULT_USER_LUCKY_NUMBER = 7;
+
+  private final Map<String, ScimUser> users = new ConcurrentHashMap<>();
+
+  private SchemaRegistry schemaRegistry;
+
+  private PatchHandler patchHandler;
+
+  @Inject
+  public InMemoryUserService(SchemaRegistry schemaRegistry, PatchHandler patchHandler) {
+    this.schemaRegistry = schemaRegistry;
+    this.patchHandler = patchHandler;
+  }
+
+  protected InMemoryUserService() {}
+
+  @PostConstruct
+  public void init() {
+    ScimUser user = new ScimUser();
+    user.setId(DEFAULT_USER_ID);
+    user.setExternalId(DEFAULT_USER_EXTERNAL_ID);
+    user.setUserName(DEFAULT_USER_EXTERNAL_ID);
+    user.setDisplayName(DEFAULT_USER_DISPLAY_NAME);
+    user.setName(new Name()
+      .setGivenName("Tester")
+      .setFamilyName("McTest"));
+    Email email = new Email();
+    email.setDisplay(DEFAULT_USER_EMAIL_VALUE);
+    email.setValue(DEFAULT_USER_EMAIL_VALUE);
+    email.setType(DEFAULT_USER_EMAIL_TYPE);
+    email.setPrimary(true);
+    user.setEmails(List.of(email));
+
+    LuckyNumberExtension luckyNumberExtension = new LuckyNumberExtension();
+    luckyNumberExtension.setLuckyNumber(DEFAULT_USER_LUCKY_NUMBER);
+
+    user.addExtension(luckyNumberExtension);
+
+    EnterpriseExtension enterpriseExtension = new EnterpriseExtension();
+    enterpriseExtension.setEmployeeNumber("12345");
+    EnterpriseExtension.Manager manager = new EnterpriseExtension.Manager();
+    manager.setValue("bulkId:qwerty");
+    enterpriseExtension.setManager(manager);
+    user.addExtension(enterpriseExtension);
+
+    users.put(user.getId(), user);
+  }
+
+  @Override
+  public Class<ScimUser> getResourceClass() {
+    return ScimUser.class;
+  }
+
+  /**
+   * @see Repository#create(ScimResource, java.util.Set, java.util.Set)
+   */
+  @Override
+  public ScimUser create(ScimUser resource, Set<AttributeReference> includedAttributes, Set<AttributeReference> excludedAttributes) throws UnableToCreateResourceException {
+    String id = UUID.randomUUID().toString();
+
+    // check to make sure the user doesn't already exist
+    boolean existingUserFound = users.values().stream()
+      .anyMatch(user -> user.getUserName().equals(resource.getUserName()));
+    if (existingUserFound) {
+      // HTTP leaking into data layer
+      throw new UnableToCreateResourceException(Response.Status.CONFLICT, "User '" + resource.getUserName() + "' already exists.");
+    }
+
+    resource.setId(id);
+    users.put(id, resource);
+    return resource;
+  }
+
+  @Override
+  public ScimUser update(String id, Set<ETag> etags, ScimUser resource, Set<AttributeReference> includedAttributeReferences, Set<AttributeReference> excludedAttributeReferences) throws ResourceException {
+    if (!users.containsKey(id)) {
+      throw new ResourceNotFoundException(id);
+    }
+    users.put(id, resource);
+    return resource;
+  }
+
+  @Override
+  public ScimUser patch(String id, Set<ETag> etags, List<PatchOperation> patchOperations, Set<AttributeReference> includedAttributeReferences, Set<AttributeReference> excludedAttributeReferences) throws ResourceException {
+    if (!users.containsKey(id)) {
+      throw new ResourceNotFoundException(id);
+    }
+    ScimUser resource = patchHandler.apply(get(id, includedAttributeReferences, excludedAttributeReferences), patchOperations);
+    users.put(id, resource);
+    return resource;
+  }
+
+
+  /**
+   * @see Repository#get(java.lang.String, java.util.Set, java.util.Set)
+   */
+  @Override
+  public ScimUser get(String id, Set<AttributeReference> includedAttributes, Set<AttributeReference> excludedAttributes) {
+    return users.get(id);
+  }
+
+  /**
+   * @see Repository#delete(java.lang.String)
+   */
+  @Override
+  public void delete(String id) throws ResourceException {
+    if (users.remove(id) == null) {
+      throw new ResourceNotFoundException(id);
+    }
+  }
+
+  /**
+   * @see Repository#find(Filter, PageRequest, SortRequest, java.util.Set, java.util.Set)
+   */
+  @Override
+  public FilterResponse<ScimUser> find(Filter filter, PageRequest pageRequest, SortRequest sortRequest, Set<AttributeReference> includedAttributes, Set<AttributeReference> excludedAttributes) {
+
+    long count = pageRequest.getCount() != null ? pageRequest.getCount() : users.size();
+    long startIndex = pageRequest.getStartIndex() != null
+      ? pageRequest.getStartIndex() - 1 // SCIM is 1-based indexed
+      : 0;
+
+    List<ScimUser> result = users.values().stream()
+      .skip(startIndex)
+      .limit(count)
+      .filter(FilterExpressions.inMemory(filter, schemaRegistry.getSchema(ScimUser.SCHEMA_URI)))
+      .collect(Collectors.toList());
+
+    return new FilterResponse<>(result, pageRequest, result.size());
+  }
+
+  /**
+   * @see Repository#getExtensionList()
+   */
+  @Override
+  public List<Class<? extends ScimExtension>> getExtensionList() {
+    return List.of(LuckyNumberExtension.class, EnterpriseExtension.class);
+  }
+}
---
+++ scim-server-examples/scim-server-jersey-4/src/test/java/org/apache/directory/scim/example/jersey4/JerseyTestServer.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.directory.scim.example.jersey4;
+
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
+import jakarta.ws.rs.SeBootstrap;
+import jakarta.ws.rs.core.UriBuilder;
+import org.apache.directory.scim.compliance.junit.EmbeddedServerExtension;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+public class JerseyTestServer implements EmbeddedServerExtension.ScimTestServer {
+
+  private SeContainer container;
+  private SeBootstrap.Instance server;
+
+  @Override
+  public URI start(int port) throws Exception {
+
+    // It doesn't look like Weld finds the beans in src/main/java, so enable implicit scanning
+    // NOTE: this isn't an issue for the scim-server tests, but those beans are located in src/test/java
+    container = SeContainerInitializer.newInstance()
+      .addPackages(true, JerseyApplication.class)
+      .initialize();
+
+    JerseyApplication app = new JerseyApplication();
+    server = SeBootstrap.start(app, SeBootstrap.Configuration.builder().port(port).build())
+      .toCompletableFuture().get(1, TimeUnit.MINUTES);
+
+    // shut down CDI container on stop
+    server.stopOnShutdown(stopResult -> container.close());
+
+    return UriBuilder.fromUri("http://localhost/").port(port).build();
+  }
+
+  @Override
+  public void shutdown() throws Exception {
+    if (server != null) {
+      server.stop().toCompletableFuture()
+        .get(10, TimeUnit.SECONDS);
+    }
+
+    if (container != null) {
+      container.close();
+    }
+  }
+}
